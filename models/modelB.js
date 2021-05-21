9ca3391247c8b47b9aea45dea28a2964ad1d5372
Nadia Naji <nadialnaji@gmail.com>
2021-05-21 11:20:58 -0600

    Create house model

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var nadiaDreamHouse = {
+  isInTheMountains: true,
+  hasFoundationProblems: false,
+  city: 'Evergreen',
+  state: 'Colorado',
+  minSquareFt: 2000,
+  maxSquareFt: 2900.99,
+  propertyFeatures: [ 'stocked pond', 'tree swing', 'shed' ],
+  houseMaterials: [ 'wood', 'glass', 'brick' ],
+};
