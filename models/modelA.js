7bb16c695804a4cfaa411d76a8e94a101707b68d
Nadia Naji <nadialnaji@gmail.com>
2021-05-21 11:18:13 -0600

    Create laptop model

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var macBookProSpecs = {
+  color: 'gray',
+  chipType: 'Apple M1 chip',
+  screenSizeInInches: 13,
+  memoryAvailableInGb: 8,
+  hasTouchBar: true,
+  hasUsbTypeA: false,
+  m1ChipSpecs: [ '8-core CPU', '8-core GPU', '16-core Neural Engine' ],
+  preInstalledSoftwareOptions: [ 'Final Cut Pro', 'LogicPro' ],
+};
