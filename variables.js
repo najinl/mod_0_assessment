c8bf137b72b9406ae67f5390364c27a8886103e3
Nadia Naji <nadialnaji@gmail.com>
2021-05-21 11:12:46 -0600

    Complete part 2

---
+++ variables.js
@@ -0,0 +1,17 @@
+var animal = "golden retriever";
+
+var isCuddly = true;
+
+animal = "guineafowl";
+
+var num8 = 8;
+
+var num3 = 3;
+
+var quotient = num3/num8;
+
+var favoriteFilms = [ "Pulp Fiction", "Before Sunrise", "The Brothers Bloom" ];
+
+favoriteFilms.shift();
+
+console.log(animal.length);
